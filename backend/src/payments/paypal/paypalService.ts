805dd9f0d6ba124ba14fdf419b8d974e93e8cd85
Willian Guedes <willian.wg70@gmail.com>
2023-03-16 18:00:16 +0000

    feat: add paypal order creation"

--- backend/src/payments/paypal/paypalService.ts
+++ backend/src/payments/paypal/paypalService.ts
@@ -1,4 +1,5 @@
 import { IPayment } from "../ipayment";
+import axios from "axios";
 
 type PaypalOrderinfo = {
     intent: "CAPTURE" | "AUTHORIZE",
@@ -11,8 +12,23 @@ type PaypalOrderinfo = {
     }[]
 }
 
+const PAYPAL_SANDBOX_URL = "https://api-m.sandbox.paypal.com";
+const PAYPAL_CREATE_ORDER_PATH = "/v2/checkout/orders";
+const access_token = process.env.PAYPAL_ACCESS_TOKEN;
+
+const headers = {
+    Authorization: `Bearer ${access_token}`
+}
+
+const api = axios.create({ baseURL: PAYPAL_SANDBOX_URL, headers });
+
+
 export class Paypal implements IPayment<PaypalOrderinfo> {
     async createOrder(orderInfo: PaypalOrderinfo): Promise<string> {
-        return orderInfo.purchase_units[0].description;
+        const response = await api.post(PAYPAL_CREATE_ORDER_PATH, orderInfo);
+
+        console.log(response.data);
+
+        return response.data.links[1].href;
     }
 }
